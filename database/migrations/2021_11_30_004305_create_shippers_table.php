1f0278a08ac60cf727a57a2707ef60d9e7328607
DimitriRomano <dimitri.romano09@gmail.com>
2021-11-30 02:03:50 +0100

    Shipper temp

---
+++ database/migrations/2021_11_30_004305_create_shippers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShippersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shippers', function (Blueprint $table) {
+            $table->id('shipper_id');
+            $table->unsignedBigInteger('order_id');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('order_id')->on('orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shippers');
+    }
+}
